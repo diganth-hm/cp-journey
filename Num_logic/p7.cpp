bac3e8573d21ff6ffb3ecba020ec03710f320fb7
Diganth <diganthhm714@gmail.com>
2026-07-19 16:36:02 +0530

    reversing a number

---
+++ Num_logic/p7.cpp
@@ -0,0 +1,20 @@
+//reverse a number  
+#include<iostream>
+using namespace std;
+
+int main()
+{
+  int num,i=0,sum=0,rem=0,rev=0;
+  cout<<"Enter the number ";
+  cin>>num;
+  while(num!=0)
+  {
+    rem=num%10;
+    rev=(rev*10)+rem;
+    num=num/10;
+   
+    
+  }
+  cout<<"The reverse of "<<num<<" number  is = "<<rev<<endl;
+  return 0;
+}
