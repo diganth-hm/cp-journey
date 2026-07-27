4d86d803815125624c1b2ea065316882c389934a
Diganth <diganthhm714@gmail.com>
2026-07-27 22:16:49 +0530

    Magic number

--- Num_logic/p20.cpp
+++ Num_logic/p20.cpp
@@ -3,26 +3,28 @@ using namespace std;
 
 int main()
 {
-    int num,org_num,sum=0,dig=0,i;
+    int num,temp,sum,rem=0,i;
     cout<<"Enter the number to be checked for magic number"<<endl;
     cin>>num;
-    org_num=num;
+    temp=num;
     if(num<0)
     {
         cout<<"The given number is not a magic number"<<endl;
     }
     else
     {
-        while(num!=0)
-        {
-            num=num/10;
-            dig++;
-        }
-        for(i=0;i<dig;i++)
-        {
-            
-        }
-        if((sum==1))
+          while (temp > 9)
+         {
+            sum=0;
+            while (temp > 0)
+             {
+                rem = temp % 10;
+                sum = sum+rem;
+                temp /= 10;
+            }
+            temp = sum;
+         }
+        if((temp==1))
         {
           cout<<"The given number is  a magic number"<<endl;
         }
